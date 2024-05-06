8f6ca83e85461114b0795660d6cce656cbf0a251
ashfouryou <116471098+ashfouryou@users.noreply.github.com>
2024-05-07 04:41:27 +0530

    Added simple tailwind css form to create vue file

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,10 +3,12 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    @vite('resources/js/app.js')
+        <!-- Include Tailwind CSS from CDN -->
+        <link href="https://cdn.jsdelivr.net/npm/tailwindcss@^2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     @inertiaHead
   </head>
-  <body>
-    @inertia
+  <body> 
+       @inertia
   </body>
 </html>
