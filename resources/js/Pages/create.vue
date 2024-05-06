3ca5eb119074ab3c62a6cd34709d88bdadc50492
ashfouryou <116471098+ashfouryou@users.noreply.github.com>
2024-05-07 04:16:14 +0530

    Route to render a sample create.vue file

---
+++ resources/js/Pages/create.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Welcome {{ name }}!</h1>
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            name: 'Ashish',
+            message: 'How are you?'
+        };
+    }
+};
+</script>
